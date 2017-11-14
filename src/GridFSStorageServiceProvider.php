5c6bb1b8f7f71d27749baba32c3ac624a1dc2dc0
Matthew Daly <matthewbdaly@gmail.com>
2017-11-14 10:47:10 +0000

    Service provider should now work as expected

---
+++ src/GridFSStorageServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Matthewbdaly\LaravelGridFSStorage;
+
+use Storage;
+use League\Flysystem\Filesystem;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\GridFS\GridFSAdapter;
+/**
+ * Service provider for GridFS Storage
+ */
+class GridFSStorageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('gridfs', function ($app, $config) {
+            $mongoClient = new MongoClient();
+            $gridFs = $mongoClient->selectDB($config['name'])->getGridFS();
+
+            $adapter = new GridFSAdapter($gridFs);
+            return new Filesystem($adapter);
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
